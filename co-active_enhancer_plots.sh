0808b1aeb4fef535aabe61f83c85831f5a6ab01f
cziyani <76186315+cziyani@users.noreply.github.com>
2022-01-10 19:43:48 +0100

    Update co-active_enhancer_plots.sh

--- co-active_enhancer_plots.sh
+++ co-active_enhancer_plots.sh
@@ -3,7 +3,7 @@
 for gene in `cat $1`
 do
 
- Rscript /Users/chaymaeziyani/Desktop/MP_new_processing_october/task5-updated/6_enhancer_activity_plots/new_file.R $gene
+ Rscript Rscript.R $gene
  
 done
 
