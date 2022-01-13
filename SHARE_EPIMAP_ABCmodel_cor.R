33720fc73047f7e5c86a83c65f86e0f8a590a4d7
cziyani <76186315+cziyani@users.noreply.github.com>
2022-01-13 23:59:26 +0100

    Add files via upload

---
+++ SHARE_EPIMAP_ABCmodel_cor.R
@@ -0,0 +1,84 @@
+
+library(data.table)
+library(ggplot2)
+require(plyr)
+library(gridExtra)
+library(grid)
+library(ggpubr)
+
+
+
+##############SHARE-EPIMAP
+##read the merge file
+final_result = fread("share_epimap_overlap_matched.out", header = F, sep = "\t")
+##reaneme col
+colnames(final_result) = c("chr", "start", "end", "gene", "cor", "tag", "chr1", "start1", "end1", "gene1", "score")
+##the nb of negative corr value
+final_result[cor > 0.5] 
+##correlation test
+cor.test(final_result$cor, final_result$score, method ="spearman") 
+##plot x/y
+p1 = ggplot(final_result, aes(x=cor, y=score) ) +
+  geom_bin2d() +
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  labs(title = "Spearman R:0.18, P-val<2.2e-16", x="SHARE-seq correlation", y="Epimap Score" )+
+  theme(plot.title=element_text( hjust=1, vjust=-7, size = 8, face="bold"),
+        aspect.ratio = 1,
+        legend.key.size = unit(0.25, 'cm'),
+        axis.title.x = element_text(colour = "black", size = 8),
+        axis.title.y = element_text(colour = "black", size = 8)
+  )
+
+
+
+##############SHARE-ABCmodel
+##read the merge file
+final_result = fread("share_abcmodel_overlap_matched.out", header = F, sep = "\t")
+##reaneme col
+colnames(final_result) = c("chr", "start", "end", "gene", "cor", "tag", "chr1", "start1", "end1", "gene1", "ABC.Score")
+##correlation test
+cor.test(final_result$cor, final_result$ABC.Score, method ="spearman") ##0.04858158
+##plot x/y
+p2= ggplot(final_result, aes(as.numeric(cor), ABC.Score)) +
+  geom_bin2d() +
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  labs(title = "Spearman R:0.048, P-val<2.2e-16", x="SHARE-seq correlation", y="ABCmodel Score" ) +
+  theme(plot.title=element_text( hjust=1, vjust=-7, size = 8, face="bold"),
+        aspect.ratio = 1,
+        legend.key.size = unit(0.25, 'cm'),
+        axis.title.x = element_text(colour = "black", size = 8),
+        axis.title.y = element_text(colour = "black", size = 8))
+
+
+
+
+################ABCmodel-EPIMAP
+##read the merge file
+final_result = fread("epimap_abcmodel_overlap_matched.out", header = F, sep = "\t")
+##add a col which contain epimap-enh info
+final_result$tag = apply(final_result, 1, function(x) paste(x[6],x[7],x[8],x[9],sep = "_"))
+##reaneme col
+colnames(final_result) = c("chr", "start", "end", "gene", "score", "chr1", "start1", "end1", "gene1", "ABC.score", "tag1")
+##correlation test
+cor.test(final_result$score, final_result$ABC.score, method ="spearman") ##0.01823965 
+##plot x/y
+p3 = ggplot(final_result, aes(score,ABC.score)) + 
+  geom_bin2d() +
+  geom_smooth(method = "lm")+
+  labs(title = "Spearman R:0.018, P-val<9.974e-11" ,x="Epimap Score", y="ABCmodel Score")+
+  theme_bw()+
+  theme(plot.title=element_text( hjust=1, vjust=-7, size = 8, face="bold"),
+        aspect.ratio = 1,
+        legend.key.size = unit(0.25, 'cm'),
+        axis.title.x = element_text(colour = "black", size = 8),
+        axis.title.y = element_text(colour = "black", size = 8),
+        
+  )
+
+
+ggarrange(p1,p2,p3, ncol = 3, nrow = 1)
+
+
+
