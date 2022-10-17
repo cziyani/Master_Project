891f7ad70eda5648977f7492c311ac9e37af1e8d
Chaymae Ziyani <76186315+cziyani@users.noreply.github.com>
2022-10-17 17:12:54 +0200

    Update Enhancers_co-activity_2.R

--- Enhancers_co-activity_2.R
+++ Enhancers_co-activity_2.R
@@ -1,4 +1,4 @@
-######## create a list of enhnacer per gene and cell
+# Script to create a list of enhancer(s) per gene and cell
 
 library(data.table)
 library(ggplot2)
@@ -8,23 +8,20 @@ library(splitstackshape)
 library(qdap)
 library(magrittr)
 
-######load and process data
-
+## Load and process data
 final_data= fread("gene_enhancer_cell.out", header = T, sep = "\t")
 
-####create a list of enhancer
+## Create a list of enhancer
 final_data1 = final_data %>% group_by(gene, cell) %>%mutate(enhancerList = toString(tag)) %>%as.data.frame()
 #final_data1 =  setDT(data1)[, list(Values=paste(V2, collapse=",")) ,c("V1","V3","V4")]
 
 final_data1 = final_data1[,-c(2)]
 final_data1 = unique(final_data1) 
 
-###count the number of cells where gene expressed and enhnacer are active 
+## Count the number of cells where simultaneously gene expressed and enhancer are active 
 s = data.table(table(final_data1$gene, final_data1$enhancerList)) 
 s = s[N > 0]
-
-####renqme col
 colnames(s) = c("gene", "enhnacerList", "NBcell")
 
-###save file
+## Save file
 write.table(s, "gene_enhanerList.out",col.names = T , row.names = F , quote = F, sep = "\t" )
