7eecf9c71aee3a324fb780db3de40d6d9dda1aee
Chaymae Ziyani <76186315+cziyani@users.noreply.github.com>
2022-10-18 19:16:20 +0200

    Update scATAC_Enhancers_process.R

--- scATAC_Enhancers_process.R
+++ scATAC_Enhancers_process.R
@@ -6,32 +6,21 @@ library(ggthemes)
 library(grid)
 library(gridExtra)
 library(ggpubr)
+ 
 
-########
-##load and process data
+
+## Load and process the data
 df = fread("peak_mergenhancer_overlap_F_0.5_sorted.out.gz", header = F, sep = "\t")
-
-##remove chr x and y
+## Remove chr x and y
 df = subset(df, V1 != "chrX" & V1 != "chrY")
-
-##create a new column containing enhancer coordinate
 df$tag = apply(df, 1, function(x) paste(x[1],x[2],x[3],sep = "_"))
-
-##rename col
 colnames(df) = c("chr", "start", "end", "cell","tag")
-
-##count how many enhancer(s) per cell in our data
 enh_per_cell = data.table(table(df$cell)) 
-
-##count how many cell(s) per enhancer in our data
 cell_per_enh = data.table(table(df$tag)) 
 
-##summary
-summary(enh_per_cell$N)
-summary(cell_per_enh)
-
-
-##plot the number of enhancers per cells
+               
+               
+## Plot the number of enhancer(s) per cell
 P1 = ggplot(enh_per_cell , aes(x=N)) + 
   geom_histogram(binwidth = 50, color="black", fill="lightblue") + 
   xlim(c(-50,2000))+
@@ -43,7 +32,7 @@ P1 = ggplot(enh_per_cell , aes(x=N)) +
 
 
 
-##plot the number of cells per enh
+## Plot the number of cell(s) per enhancer
 P2=ggplot(cell_per_enh , aes(x=N)) +
   geom_histogram(binwidth = 50, color="black", fill="orange", alpha = 0.4) +
   xlim(c(-50,3000))+ 
@@ -55,8 +44,7 @@ P2=ggplot(cell_per_enh , aes(x=N)) +
 
 
 
-###put both plots in the same page
+## Arrange both plots on the same page
 ggarrange(P1, P2 , 
           labels = c("A", "B"),
           ncol = 2, nrow = 1)
-
