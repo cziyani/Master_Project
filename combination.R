9721b70880bfb3dd96818d626e2a9bba7a2de90c
Chaymae Ziyani <76186315+cziyani@users.noreply.github.com>
2022-10-18 19:13:38 +0200

    Update combination.R

--- combination.R
+++ combination.R
@@ -1,5 +1,4 @@
-######## explore observed and expected enhancers combinations
-
+# Script to explore observed and expected enhancers combinations
 
 library(data.table)
 library(ggplot2)
@@ -12,8 +11,7 @@ library(purrr)
 library(ggpubr)
 
 
-######load and process data
-
+## Load and process SHARE-seq coex data : List of enhancers per cell
 df= fread("gene_enhanerList.out", header = T, sep = "\t")
 df1 = data.table(table(df$gene)) 
 colnames(df1) = c("gene", "obs_comb")
@@ -21,42 +19,29 @@ df$count = lengths(strsplit(df$enhnacerList, split = ','))
 max(df$count) ##23
 
 
-dt= fread("gene_enhancer_cell.out", header = T, sep = "\t") ##2224692 associations
-##remove the cell column
+
+## Load and process SHARE-seq coex data:
+dt= fread("gene_enhancer_cell.out", header = T, sep = "\t") 
 dt$cell = NULL
-##keep unique associations
 dt = unique(dt) 
-##data table on the gene
 dt1 = data.table(table(dt$gene)) 
-## keep gene that have more than 1 enhancer
-###because we are interested by stydyin g co-active enhancer within the same gene
+## We are interested in studyin co-active enhancer within the same gene so we're keeping only genes that have more than 1 enhancer
 dt1 = dt1[N > 1]
-
-##factorial : expected combinations
+## Factorial : expected combinations
 dt1$fact = as.integer(factorial(dt1$N))+1
-
-##remove the NA
 dt1 = dt1[!is.na(dt1$fact)] ##remove NA because we have some gene that have 20 enhancer
 colnames(dt1) = c("gene","nb_enh","exp_comb")
-
-##merge
 dt_final = merge(df1, dt1, by = "gene")
-
-##check if the oberved combinations are not bigger than the expected ones 
+## Validation
 dt_final[obs_comb > exp_comb]
 
-##density plot
+
+## Density-plot
 d1 = data.table(dt_final$gene, stack(dt_final[,2:4]))
-summary(d1)
-
 d1 = d1[ind != "nb_enh"]
-
 colnames(d1)[3] = "Group"
-
 ##apply a wilcoxon test
 wilcox.test(df1$obs_comb, dt1$exp_comb)
-
-###plot
 ggplot(d1, aes(x=log10(values), fill = Group)) + 
     geom_density(stat = "density", alpha = 0.5, adjust = 5) +  
   scale_fill_brewer(palette = "Set2")+
@@ -72,11 +57,9 @@ ggplot(d1, aes(x=log10(values), fill = Group)) +
   
 
 
-#####explore and validate previous results
-
+## Validate previous results
 dt_final$obs_log = log10(dt_final$obs_comb)
 dt_final$exp_log = log10(dt_final$exp_comb)
-
 unique(dt_final$exp_comb)
 
 dp2 = ggplot( dt_final, aes(x = obs_comb, y = exp_comb ))  +
@@ -92,23 +75,5 @@ dp2 = ggplot( dt_final, aes(x = obs_comb, y = exp_comb ))  +
         axis.line = element_line(colour = "black", size = 1),
         panel.border = element_rect(colour = "black", fill=NA, size=1))
 
-
-
 ggplot(dt_final, aes(x=obs_comb, y = exp_comb)) + 
   geom_point(position = "jitter")
-
-##create a data table on exp
-c = data.table(table(dt_final$exp_comb))
-
-##kepp genes that have 3 or 4 enh
-d = dt_final[nb_enh == 4] ##600 gene that have 4 enhancers
-
-##keep only the cases when the obs_comb is too diff from the exp_comb
-d2 = d[obs_comb != exp_comb] ##214 gene that have 3/4 enhancers whre the exp combination
-#are diff from the observed ones
-
-##keep only the gene column
-d2 = d2[,.(gene)]
-
-##save it
-write.table(d2, "gene_list",col.names = T , row.names = F , quote = F, sep = "\t" )
