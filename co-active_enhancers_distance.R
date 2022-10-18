aa4675e504fc319e68580aeef50880ad7cdcb082
Chaymae Ziyani <76186315+cziyani@users.noreply.github.com>
2022-10-18 18:52:46 +0200

    Update co-active_enhancers_distance.R

--- co-active_enhancers_distance.R
+++ co-active_enhancers_distance.R
@@ -1,4 +1,4 @@
-######### Explore distance between pairs of co-active enhancers
+# Script to explore distance between pairs of co-active enhancers
 
 library(data.table)
 library(ggplot2)
@@ -6,55 +6,37 @@ library(dplyr)
 library(plyr)
 library(ggthemes)
 
-##read enhancer-list file 
-data = fread("gene_enhanerList.out",header=T, stringsAsFactors = FALSE) ## 462629 Vs 168750
-
-##keep only unique entries
+## Load and process the SHARE-seq coex data : list of enhancer per gene 
+data = fread("gene_enhanerList.out",header=T, stringsAsFactors = FALSE) 
 data=unique(data)
-
-##remove gene col
 data$gene = NULL
-
-##keep only genes that have 2 enh
 data$count =lengths(strsplit(data$enhnacerList, split = ','))
-
-data = data[count==2]
-
-##separate enh col
+## keep only genes that have 2 enh
+data = data[count == 2]
+## Create a column for each enhancer in the list
 data$enh1 = data.table(unlist(lapply(data$enhnacerList, function(x) unlist(strsplit(x,"[,]"))[1])))$V1
 data$enh2 = data.table(unlist(lapply(data$enhnacerList, function(x) unlist(strsplit(x,"[,]"))[2])))$V1
-
-##remove the first col
-data$enhnacerList=NULL
-
+data$enhnacerList=NULL                                   
 data$start1= data.table(unlist(lapply(data$enh1, function(x) unlist(strsplit(x,"[_]"))[2])))$V1
 data$end1 = data.table(unlist(lapply(data$enh1, function(x) unlist(strsplit(x,"[_]"))[3])))$V1
 data$start2 = data.table(unlist(lapply(data$enh2, function(x) unlist(strsplit(x,"[_]"))[2])))$V1
 data$end2 = data.table(unlist(lapply(data$enh2, function(x) unlist(strsplit(x,"[_]"))[3])))$V1
-
-##create a  other data.table
 distanceData =  data[,.(start1,end1, start2,end2, NBcell,count )]
 
-##calculate the midpoint 
+## Calculate the midpoint between pairs of enhancers for Distnace calculation
 distanceData$mean1 = (as.numeric(distanceData$start1)+ as.numeric(distanceData$end1)) / 2
 distanceData$mean2 = (as.numeric(distanceData$start2)+ as.numeric(distanceData$end2)) / 2
-
 distanceData = distanceData[,-c(1:4)]
-
-
-##sum up cells
+## Sum up cells
 dt2 = distanceData[, sum(NBcell), by=c("mean1", "mean2", "count")]
-
-##calculate distance
+## Calculate the distance between pairs of enhnacers regulating the same gene
 dt2$distance = abs(as.numeric(dt2$mean2) - as.numeric(dt2$mean1))
 
-##plot
+## Box-plot
 dt2$bins<- cut(as.numeric(dt2$distance), breaks = c(0, 50000, 100000, 200000, 500000, 1000000, 1500000,2000000),
                labels = c("0-50KB", "50KB-100KB", "100KB-200KB", "200KB-500KB", "500KB-1000KB", "1000KB-1500KB", "1500KB-2000KB" ),
                include.lowest = TRUE)
-##rename the col
 colnames(dt2) = c("mean1", "mean2","count" ,"NBcell", "distance", "bins")
-
 
 
 df <- ddply(dt2,
@@ -67,12 +49,8 @@ df <- ddply(dt2,
             max= max(NBcell),
             lab = length(bins))
 
-
-
 colnames(dt2)[6] = "Distance"
 
-
-##plot
 ggplot(dt2, aes(dt2$Distance,dt2$NBcell, fill=Distance)) +
   geom_boxplot(alpha = 0.4)+
   scale_y_continuous(limits=c(0,100))+
@@ -85,7 +63,3 @@ ggplot(dt2, aes(dt2$Distance,dt2$NBcell, fill=Distance)) +
   theme(plot.title=element_text( hjust=1, vjust=-17, size = 10, face="bold"),
         axis.title.x = element_text(colour = "black", face="bold"),
         axis.title.y = element_text(colour = "black", face="bold"))
-
-
-
-
