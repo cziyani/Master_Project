7f5a5e9e16296c8b9adc77ae454a6d84dfb76931
Chaymae Ziyani <76186315+cziyani@users.noreply.github.com>
2022-10-18 19:18:21 +0200

    Update scATAC_EpiMap_enhancer_distribution.R

--- scATAC_EpiMap_enhancer_distribution.R
+++ scATAC_EpiMap_enhancer_distribution.R
@@ -1,22 +1,21 @@
-##########explore EpiMap enhancer after merging as well as scATAC_seq enhancer distribution
+# Script to explore EpiMap enhancer as well as scATAC_seq enhancer distribution
 
-##add the packages
 library(data.table)
 library(ggplot2)
 
-##epimap-enhancer
+## Load and process Epimap-enhancer data
 df = fread("atac_peak_file.bed", header = F, sep = "\t")
 colnames(df) = c("chr", "start", "end")
 
-###plot
+## Plot the distribution
 df$size = abs(df$end - df$start)
 dp = ggplot(df  , aes(x=size)) + geom_histogram(bins = 100) 
 summary(df$size)
 
-##atac-enhancer
+## Load and process SHARE-seq scATAC-seq enhancer 
 dt = fread("enhancers_processing_task1_sorted_merge.bed", header = F, sep = "\t")
 colnames(dt) = c("chr", "start", "end")
 
-###plot
+## Plot the distribution
 dt$size = abs(dt$end - dt$start)
 dp1 = ggplot(dt  , aes(x=size)) + geom_histogram(bins = 100) 
